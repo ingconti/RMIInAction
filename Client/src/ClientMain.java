e410bc70a43e7d3269bbb735c5c5f0cdfd95b193
Gian Enrico Conti <ingconti@ingconti.com>
2018-04-30 13:04:18 +0200

    added classes

--- Client/src/ClientMain.java
+++ Client/src/ClientMain.java
@@ -1,6 +1,35 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import static java.lang.System.*;
+
 public class ClientMain {
 
+    private ClientMain() {}
+
     public static void main(String[] args) {
-        System.out.println("Hello From Client!");
+        //out.println("Hello From Client!");
+
+        try {
+            // Getting the registry
+            Registry registry = LocateRegistry.getRegistry(null);
+
+            // Looking up the registry for the remote object
+            Loggable stub = (Loggable) registry.lookup("Loggable");
+
+            // Calling the remote method using the obtained object
+            //stub.printMsg();
+
+            // System.out.println("Remote method invoked");
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+
     }
+
+
+
+
+
 }
--- Server/src/ServerMain.java
+++ Server/src/ServerMain.java
@@ -1,20 +1,39 @@
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerMain implements Loggable {
 
+    public ServerMain() {
 
-    public static void main(String[] args) {
-        System.out.println("Hello from Server!");
-
-        Loggable stub = (Loggable) UnicastRemoteObject.exportObject(this, 0);
     }
 
-    @Override
+    static int PORT = 1234;
+
+    public static void main(String[] args) {
+        //System.out.println("Hello from Server!");
+
+        try {
+            ServerMain obj = new ServerMain();
+            Loggable stub = (Loggable) UnicastRemoteObject.exportObject(obj, PORT);
+
+            // Bind the remote object's stub in the registry
+           //NO Registry registry = LocateRegistry.getRegistry();
+            Registry registry = LocateRegistry.createRegistry(PORT);
+            registry.bind("Loggable", stub);
+            System.err.println("Server ready");
+
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
     public boolean login(String nick) throws RemoteException {
         return false;
     }
 
-    @Override
     public void logout(String nick) throws RemoteException {
 
     }
